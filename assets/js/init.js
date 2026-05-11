f34e0cf17c287394250897e27a2083b53ea629fb
Dean <deaneryfb@gmail.com>
2026-05-11 05:13:43 -0600

    ¨fixed¨

--- assets/js/init.js
+++ assets/js/init.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
         new Splide(slider, {
             type: 'loop',
             perPage: 1,
-            autoplay: true,
-            interval: 10000,
+            autoplay: false,
+            interval: 15000,
             pauseOnHover: true,
             pauseOnFocus: true,
             arrows: true,
             pagination: true,
-            speed: 900,
+            speed: 1000,
             easing: 'cubic-bezier(0.25, 1, 0.5, 1)',
         }).mount();
     });
